ed02fb328d4e485ab79ad92ea2890c4bde7b22f3
Alan Michael <alanmichael12345@gmail.com>
2023-01-16 19:34:38 -0300

    Project created

---
+++ MauiAppOne/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiAppOne;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiAppOne/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiAppOne;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
