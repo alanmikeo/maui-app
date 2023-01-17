9c0ebb3a1ef822608be6e8aff3073ceef7d46a43
Alan Michael <alanmichael12345@gmail.com>
2023-01-17 17:53:49 -0300

    added material icons into uranium ui

--- MauiAppOne/MauiProgram.cs
+++ MauiAppOne/MauiProgram.cs
@@ -16,6 +16,7 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddMaterialIconFonts();
 			}).ConfigureMauiHandlers(handlers =>
             {
                 handlers.AddUraniumUIHandlers(); // 👈 This line should be added.
